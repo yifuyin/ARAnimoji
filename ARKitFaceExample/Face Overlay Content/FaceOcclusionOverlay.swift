9351a617bfba989159f2899c24fe3e989ebf4ab0
yifuyin <yifuyin7@gmail.com>
2023-04-07 09:20:40 -0700

    info

---
+++ ARKitFaceExample/Face Overlay Content/FaceOcclusionOverlay.swift
@@ -0,0 +1,53 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Demonstrates how to simulate occlusion of virtual content by the real-world face.
+*/
+
+import ARKit
+import SceneKit
+
+class FaceOcclusionOverlay: NSObject, VirtualContentController {
+    var contentNode: SCNNode?
+    
+    private var originalJawY: Float = 0
+    
+    private lazy var jawNode = contentNode!.childNode(withName: "jaw", recursively: true)!
+    private lazy var eyeLeftNode = contentNode!.childNode(withName: "eyeLeft", recursively: true)!
+    private lazy var eyeRightNode = contentNode!.childNode(withName: "eyeRight", recursively: true)!
+    
+    private lazy var jawHeight: Float = {
+        let (min, max) = jawNode.boundingBox
+        return max.y - min.y
+    }()
+
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard anchor is ARFaceAnchor else { return nil }
+
+        contentNode = SCNReferenceNode(named: "robotHead")
+        originalJawY = jawNode.position.y
+        
+        // Assign a random color to the eyes.
+        let material = SCNMaterial.materialWithColor(anchor.identifier.toRandomColor())
+        contentNode?.childNode(withName: "eyeLeft", recursively: true)?.geometry?.materials = [material]
+        contentNode?.childNode(withName: "eyeRight", recursively: true)?.geometry?.materials = [material]
+        
+        return contentNode
+    }
+    
+    /// - Tag: BlendShapeAnimation
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let faceAnchor = anchor as? ARFaceAnchor
+            else { return }
+        
+        let blendShapes = faceAnchor.blendShapes
+        guard let eyeBlinkLeft = blendShapes[.eyeBlinkLeft] as? Float,
+            let eyeBlinkRight = blendShapes[.eyeBlinkRight] as? Float,
+            let jawOpen = blendShapes[.jawOpen] as? Float
+            else { return }
+        eyeLeftNode.scale.z = 1 - eyeBlinkLeft
+        eyeRightNode.scale.z = 1 - eyeBlinkRight
+        jawNode.position.y = originalJawY - jawHeight * jawOpen
+    }
+}
